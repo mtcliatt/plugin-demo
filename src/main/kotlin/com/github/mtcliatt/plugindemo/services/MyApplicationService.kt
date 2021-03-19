12b2681737219185f9dd335c6ab4d39c6d36e6c1
GitHub Action <action@github.com>
2021-03-19 17:34:51 +0000

    Template cleanup

--- src/main/kotlin/com/github/mtcliatt/plugindemo/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/mtcliatt/plugindemo/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.mtcliatt.plugindemo.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.mtcliatt.plugindemo.MyBundle
 
 class MyApplicationService {
 
